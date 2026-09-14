e2a16925ff890e2f5650df6d909c379cb77d42dd
Chocobe <kyw05171@gmail.com>
2026-09-14 15:55:07 +0900

    feat: connect backend to mysql

--- back/src/main/java/com/budzet/BudzetApplication.java
+++ back/src/main/java/com/budzet/BudzetApplication.java
@@ -2,15 +2,12 @@ package com.budzet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-        // TODO: MySQL 연동 후, 삭제하기
-        exclude = DataSourceAutoConfiguration.class
-)
+@SpringBootApplication
 public class BudzetApplication {
 
     public static void main(String[] args) {
+        System.out.println("Javachip 입니다.");
         SpringApplication.run(BudzetApplication.class, args);
     }
 
