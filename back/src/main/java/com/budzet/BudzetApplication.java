b973f51464775ed0e7ae6ccd90c99a174da513bf
Chocobe <kyw05171@gmail.com>
2026-09-14 10:37:07 +0900

    refactor: init project configs

--- back/src/main/java/com/budzet/BudzetApplication.java
+++ back/src/main/java/com/budzet/BudzetApplication.java
@@ -2,8 +2,12 @@ package com.budzet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        // TODO: MySQL 연동 후, 삭제하기
+        exclude = DataSourceAutoConfiguration.class
+)
 public class BudzetApplication {
 
     public static void main(String[] args) {
