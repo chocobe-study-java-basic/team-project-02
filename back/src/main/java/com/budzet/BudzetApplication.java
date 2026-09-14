1adb278b4db347c155b649b36c9843f5490141c9
Chocobe <kyw05171@gmail.com>
2026-09-14 16:09:12 +0900

    feat: remove System.out.println()

--- back/src/main/java/com/budzet/BudzetApplication.java
+++ back/src/main/java/com/budzet/BudzetApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BudzetApplication {
 
     public static void main(String[] args) {
-        System.out.println("Javachip 입니다.");
         SpringApplication.run(BudzetApplication.class, args);
     }
 
